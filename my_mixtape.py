3962e1b10dc4aa9d0da658256887a2867e20f49b
AmbroseEXE <145411442+AmbroseEXE@users.noreply.github.com>
2024-09-16 08:02:03 -0400

    Create my_mixtape.py

---
+++ my_mixtape.py
@@ -0,0 +1,6 @@
+#my_mixtape.py
+
+playlist = ['The Crowing - Coheed & Cambria', 'Cold - Crossfade', 'Duality - Slipknot', 'Intergalactic - Beastie Boys', 'Still Waiting - Sum 41', 'Crawling - Linkin Park']
+
+for song in range(len(playlist)):
+  print(playlist[song])
